7cdfedf09351e19b6c697be260fa636e73934062
sihoon <sihoon1223@naver.com>
2020-03-19 13:25:25 +0900

    component 성능 최적화

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,20 +7,29 @@ import {
 } from 'react-icons/md';
 import cn from 'classnames';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
 
   return (
-    <div className="TodoListItem">
-      <div className={cn('checkBox', { checked })} onClick={() => onToggle(id)}>
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove" onClick={() => onRemove(id)}>
-        <MdRemoveCircleOutline />
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div
+          className={cn('checkBox', { checked })}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={() => onRemove(id)}>
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </div>
   );
 };
 
-export default TodoListItem;
+//props ( todo,onRemove,onToggle이 바뀌지 않으면 리랜더링 X)
+export default React.memo(
+  TodoListItem,
+  (prevProps, nextProps) => prevProps.todo === nextProps.todo,
+);
